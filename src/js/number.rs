74c76c729b843dc34af843e42c6557e99547cda8
Tom Bebbington <tophattedcoder@gmail.com>
2014-04-10 16:46:25 +0100

    Use to_value and from_value in Number

--- src/js/number.rs
+++ src/js/number.rs
@@ -1,4 +1,4 @@
-use js::value::{Value, ResultValue, VFunction, VNumber, VInteger, VObject, VBoolean, to_value};
+use js::value::{Value, ResultValue, VFunction, VNumber, VInteger, VObject, VBoolean, to_value, from_value};
 use js::function::{NativeFunc, NativeFunction};
 use collections::treemap::TreeMap;
 use std::gc::Gc;
@@ -6,63 +6,61 @@ use std::cell::RefCell;
 use std::f64::{NAN, MAX_VALUE, MIN_VALUE, INFINITY, NEG_INFINITY, EPSILON};
 /// Parse a float
 pub fn parse_float(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
-	let parsed = from_str(args.get(0).borrow().to_str());
-	return Ok(Gc::new(VNumber(match parsed {
+	let parsed = from_str::<f64>(from_value::<~str>(*args.get(0)).unwrap());
+	return Ok(to_value(match parsed {
 		Some(v) => v,
 		None => NAN
-	})));
+	}));
 }
 /// Parse an int
 pub fn parse_int(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
-	let parsed = from_str(args.get(0).borrow().to_str());
-	return Ok(Gc::new(match parsed {
-		Some(v) => VInteger(v),
-		None => VNumber(NAN)
-	}));
+	let parsed = from_str::<i32>(from_value::<~str>(*args.get(0)).unwrap());
+	return Ok(match parsed {
+		Some(v) => to_value(v),
+		None => to_value(NAN)
+	});
 }
 /// Check if a value when converted to a number is finite
 pub fn is_finite(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
-	if args.len() == 0 {
-		Ok(Gc::new(VBoolean(false)))
+	Ok(to_value(if args.len() == 0 {
+		false
 	} else {
-		let num = args.get(0).borrow().to_num();
-		Ok(Gc::new(VBoolean(num.is_finite())))
-	}
+		from_value::<f64>(*args.get(0)).unwrap().is_finite()
+	}))
 }
 /// Check if a number is finite
 pub fn strict_is_finite(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
-	if args.len() == 0 {
-		Ok(Gc::new(VBoolean(false)))
+	Ok(to_value(if args.len() == 0 {
+		false
 	} else {
-	let num = args.get(0).borrow();
-		Ok(Gc::new(VBoolean(match *num {
+		let num = args.get(0).borrow();
+		match *num {
 			VNumber(v) => v.is_finite(),
 			VInteger(_) => true, // integers can't be infinite
 			_ => false
-		})))
-	}
+		}
+	}))
 }
 /// Check if a value when converted to a number is equal to NaN
 pub fn is_nan(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
-	if args.len() == 0 {
-		Ok(Gc::new(VBoolean(false)))
+	Ok(to_value(if args.len() == 0 {
+		false
 	} else {
-		let num = args.get(0).borrow().to_num();
-		Ok(Gc::new(VBoolean(num.is_nan())))
-	}
+		from_value::<f64>(*args.get(0)).unwrap().is_nan()
+	}))
 }
 /// Check if a number is equal to NaN
 pub fn strict_is_nan(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
-	if args.len() == 0 {
-		Ok(Gc::new(VBoolean(false)))
+	Ok(to_value(if args.len() == 0 {
+		false
 	} else {
-	let num = args.get(0).borrow();
-		Ok(Gc::new(VBoolean(match *num {
+		let num = args.get(0).borrow();
+		match *num {
 			VNumber(v) => v.is_nan(),
 			VInteger(_) => true, // integers can't be NaN
 			_ => false
-		})))
-	}
+		}
+	}))
 }
 /// Initialise the parse functions on a global object
 pub fn init(obj:Value) {
