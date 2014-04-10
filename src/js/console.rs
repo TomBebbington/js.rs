b5316bb956c82b482cc2d865689d986d025dd0c4
Tom Bebbington <tophattedcoder@gmail.com>
2014-04-10 16:06:04 +0100

    Console.log accepts multiple arguments

--- src/js/console.rs
+++ src/js/console.rs
@@ -1,12 +1,14 @@
 use js::function::{NativeFunction, NativeFunc};
-use js::value::{Value, ResultValue, VFunction, VUndefined, VObject, to_value};
+use js::value::{Value, ResultValue, VFunction, VUndefined, VObject, to_value, from_value};
 use collections::treemap::TreeMap;
 use std::gc::Gc;
 use std::cell::RefCell;
+use std::iter::FromIterator;
 use time::{now, strftime};
 /// Print a javascript value to stdout
 pub fn log(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
-	println!("{}: {}", strftime("%X", &now()), args.get(0).borrow());
+	let args : Vec<~str> = FromIterator::from_iter(args.iter().map(|x|from_value::<~str>(*x).unwrap()));
+	println!("{}: {}", strftime("%X", &now()), args.connect(" "));
 	return Ok(Gc::new(VUndefined));
 }
 /// Create a new 'console' object
