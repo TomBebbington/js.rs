608feec35ef8682be0f3eafea4a2a25d1f4e345c
Tom Bebbington <tophattedcoder@gmail.com>
2014-04-10 11:20:07 +0100

    Add Number constants

--- src/js/number.rs
+++ src/js/number.rs
@@ -3,7 +3,7 @@ use js::function::{NativeFunc, NativeFunction};
 use collections::treemap::TreeMap;
 use std::gc::Gc;
 use std::cell::RefCell;
-use std::f64::NAN;
+use std::f64::{NAN, MAX_VALUE, MIN_VALUE, INFINITY, NEG_INFINITY, EPSILON};
 /// Parse a float
 pub fn parse_float(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
 	let parsed = from_str(args.get(0).borrow().to_str());
@@ -51,6 +51,12 @@ pub fn init(obj:Value) {
 /// Create a new 'Number' object
 pub fn _create() -> Value {
 	let mut number = TreeMap::new();
+	number.insert(~"NaN", Gc::new(VNumber(NAN)));
+	number.insert(~"MAX_VALUE", Gc::new(VNumber(MAX_VALUE)));
+	number.insert(~"MIN_VALUE", Gc::new(VNumber(MIN_VALUE)));
+	number.insert(~"POSITIVE_INFINITY", Gc::new(VNumber(INFINITY)));
+	number.insert(~"NEGATIVE_INFINITY", Gc::new(VNumber(NEG_INFINITY)));
+	number.insert(~"EPSILON", Gc::new(VNumber(EPSILON)));
 	number.insert(~"parseFloat", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(parse_float, 1))))));
 	number.insert(~"parseInt", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(parse_int, 1))))));
 	number.insert(~"isFinite", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(strict_is_finite, 1))))));
