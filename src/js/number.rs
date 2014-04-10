6a8c3f7c34858254d74e15d41a627c4f358c362a
Tom Bebbington <tophattedcoder@gmail.com>
2014-04-10 11:15:04 +0100

    Add isFinite and Number.isFinite

--- src/js/number.rs
+++ src/js/number.rs
@@ -1,4 +1,4 @@
-use js::value::{Value, ResultValue, VFunction, VNumber, VInteger, VObject};
+use js::value::{Value, ResultValue, VFunction, VNumber, VInteger, VObject, VBoolean};
 use js::function::{NativeFunc, NativeFunction};
 use collections::treemap::TreeMap;
 use std::gc::Gc;
@@ -20,15 +20,39 @@ pub fn parse_int(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
 		None => VNumber(NAN)
 	}));
 }
+/// Check if a value when converted to a number is finite
+pub fn is_finite(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
+	if args.len() == 0 {
+		Ok(Gc::new(VBoolean(false)))
+	} else {
+		let num = args.get(0).borrow().to_num();
+		Ok(Gc::new(VBoolean(num.is_finite())))
+	}
+}
+/// Check if a number is finite
+pub fn strict_is_finite(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
+	if args.len() == 0 {
+		Ok(Gc::new(VBoolean(false)))
+	} else {
+	let num = args.get(0).borrow();
+		Ok(Gc::new(VBoolean(match *num {
+			VNumber(v) => v.is_finite(),
+			VInteger(_) => true, // integers can't be infinite
+			_ => false
+		})))
+	}
+}
 /// Initialise the parse functions on a global object
 pub fn init(obj:Value) {
 	obj.borrow().set_field(~"parseFloat", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(parse_float, 1))))));
 	obj.borrow().set_field(~"parseInt", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(parse_int, 1))))));
+	obj.borrow().set_field(~"isFinite", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(is_finite, 1))))));
 }
 /// Create a new 'Number' object
 pub fn _create() -> Value {
 	let mut number = TreeMap::new();
 	number.insert(~"parseFloat", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(parse_float, 1))))));
 	number.insert(~"parseInt", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(parse_int, 1))))));
+	number.insert(~"isFinite", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(strict_is_finite, 1))))));
 	Gc::new(VObject(RefCell::new(number)))
 }
