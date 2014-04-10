6b1265f92022840dc287d190a889b38f589bc3af
Tom Bebbington <tophattedcoder@gmail.com>
2014-04-10 11:32:33 +0100

    Add isNaN and Number.isNaN

--- src/js/number.rs
+++ src/js/number.rs
@@ -42,6 +42,28 @@ pub fn strict_is_finite(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
 		})))
 	}
 }
+/// Check if a value when converted to a number is equal to NaN
+pub fn is_nan(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
+	if args.len() == 0 {
+		Ok(Gc::new(VBoolean(false)))
+	} else {
+		let num = args.get(0).borrow().to_num();
+		Ok(Gc::new(VBoolean(num.is_nan())))
+	}
+}
+/// Check if a number is equal to NaN
+pub fn strict_is_nan(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
+	if args.len() == 0 {
+		Ok(Gc::new(VBoolean(false)))
+	} else {
+	let num = args.get(0).borrow();
+		Ok(Gc::new(VBoolean(match *num {
+			VNumber(v) => v.is_nan(),
+			VInteger(_) => true, // integers can't be NaN
+			_ => false
+		})))
+	}
+}
 /// Initialise the parse functions on a global object
 pub fn init(obj:Value) {
 	obj.borrow().set_field(~"NaN", Gc::new(VNumber(NAN)));
@@ -49,6 +71,7 @@ pub fn init(obj:Value) {
 	obj.borrow().set_field(~"parseFloat", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(parse_float, 1))))));
 	obj.borrow().set_field(~"parseInt", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(parse_int, 1))))));
 	obj.borrow().set_field(~"isFinite", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(is_finite, 1))))));
+	obj.borrow().set_field(~"isNaN", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(is_nan, 1))))));
 }
 /// Create a new 'Number' object
 pub fn _create() -> Value {
@@ -62,5 +85,6 @@ pub fn _create() -> Value {
 	number.insert(~"parseFloat", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(parse_float, 1))))));
 	number.insert(~"parseInt", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(parse_int, 1))))));
 	number.insert(~"isFinite", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(strict_is_finite, 1))))));
+	number.insert(~"isNaN", Gc::new(VFunction(RefCell::new(NativeFunc(NativeFunction::new(strict_is_nan, 1))))));
 	Gc::new(VObject(RefCell::new(number)))
 }
