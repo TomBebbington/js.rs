dfae32aa7818bebd8b3ab850eb648628ebdc1e39
Tom Bebbington <tophattedcoder@gmail.com>
2014-04-10 17:20:05 +0100

    Add console.error and console.exception

--- src/js/console.rs
+++ src/js/console.rs
@@ -4,6 +4,8 @@ use collections::treemap::TreeMap;
 use std::gc::Gc;
 use std::cell::RefCell;
 use std::iter::FromIterator;
+use std::io::stdio::stderr;
+use std::io::Writer;
 use time::{now, strftime};
 /// Print a javascript value to stdout
 pub fn log(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
@@ -11,9 +13,19 @@ pub fn log(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
 	println!("{}: {}", strftime("%X", &now()), args.connect(" "));
 	return Ok(Gc::new(VUndefined));
 }
+/// Print a javascript value to stderr
+pub fn error(_:Value, _:Value, args:Vec<Value>) -> ResultValue {
+	let args : Vec<~str> = FromIterator::from_iter(args.iter().map(|x|from_value::<~str>(*x).unwrap()));
+	match writeln!(&mut stderr().unwrap(), "{}: {}", strftime("%X", &now()), args.connect(" ")) {
+		Ok(_) => Ok(Gc::new(VUndefined)),
+		Err(io_error) => Err(to_value(io_error.to_str()))
+	}
+}
 /// Create a new 'console' object
 pub fn _create() -> Value {
 	let mut console = TreeMap::new();
 	console.insert(~"log", to_value(log));
+	console.insert(~"error", to_value(error));
+	console.insert(~"exception", to_value(error));
 	to_value(console)
 }
