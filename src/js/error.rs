b8a57d75f01818b54869d03ae12593bb6ef80e1c
Tom Bebbington <tophattedcoder@gmail.com>
2014-04-10 16:09:56 +0100

    Use to_value where applicable in Error

--- src/js/error.rs
+++ src/js/error.rs
@@ -8,7 +8,7 @@ use std::cell::RefCell;
 /// Create a new error
 pub fn make_error(this:Value, _:Value, args:Vec<Value>) -> ResultValue {
 	if args.len() >= 1 {
-		this.borrow().set_field(~"message", Gc::new(VString(args.get(0).borrow().to_str())));
+		this.borrow().set_field(~"message", to_value(args.get(0).borrow().to_str()));
 	}
 	Ok(Gc::new(VUndefined))
 }
@@ -16,14 +16,14 @@ pub fn make_error(this:Value, _:Value, args:Vec<Value>) -> ResultValue {
 pub fn to_string(this:Value, _:Value, _:Vec<Value>) -> ResultValue {
 	let name = this.borrow().get_field(~"name");
 	let message = this.borrow().get_field(~"message");
-	Ok(Gc::new(VString(format!("{}: {}", name.borrow(), message.borrow()))))
+	Ok(to_value(format!("{}: {}", name.borrow(), message.borrow())))
 }
 /// Create a new 'Error' object
 pub fn _create() -> Value {
 	let mut func = NativeFunction::new(make_error, 0);
 	let mut prototype : ObjectData = TreeMap::new();
-	prototype.insert(~"message", Gc::new(VString(~"")));
-	prototype.insert(~"name", Gc::new(VString(~"Error")));
+	prototype.insert(~"message", to_value(~""));
+	prototype.insert(~"name", to_value(~"Error"));
 	prototype.insert(~"toString", to_value(to_string));
 	func.object.insert(~"prototype", to_value(prototype));
 	Gc::new(VFunction(RefCell::new(NativeFunc(func))))
