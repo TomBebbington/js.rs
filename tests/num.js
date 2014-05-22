0b4b19ee21a49978a8c930b725bed884f7809526
Tom Bebbington <tophattedcoder@gmail.com>
2014-05-22 18:33:59 +0100

    Update number tests

--- tests/num.js
+++ tests/num.js
@@ -1,7 +1,10 @@
 // @description Number unit tests
-assert(1 + 2 == 3, "Number equality");
+assert(3.0 == 3, "Number equality");
+assert("3" == 3, "Number and string equality");
+assert("4" !== 3, "Number and string strict equality");
 assert(Number.isFinite(2), "Number.isFinite");
-assert(5 + 2 == 7, "Number addition");
-assert(5 - 2 == 3, "Number subtraction");
+assert(!Number.isFinite(NaN), "Number.isFinite");
+assert((5 + 2) == 7, "Number addition");
+assert((5 - 2) == 3, "Number subtraction");
 assert((5 / 2) == 2.5, "Number division");
 assert((5 * 2) == 10, "Number multiplication");
