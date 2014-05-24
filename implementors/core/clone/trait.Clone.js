7a43c8fdefc8afd17f04cbbd5ebc34feb6ece245
Tom Bebbington <tophattedcoder@gmail.com>
2014-05-24 17:06:25 +0100

    Update pages

---
+++ implementors/core/clone/trait.Clone.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['script'] = {Property:"script/js/object/struct.Property.html",NativeFunction:"script/js/function/struct.NativeFunction.html",RegularFunction:"script/js/function/struct.RegularFunction.html",Function:"script/js/function/type.Function.html",ValueData:"script/js/value/type.ValueData.html",Interpreter:"script/exec/struct.Interpreter.html",ParseError:"script/parser/type.ParseError.html",NumberType:"script/lexer/type.NumberType.html",CommentType:"script/lexer/type.CommentType.html",StringType:"script/lexer/type.StringType.html",TokenData:"script/ast/type.TokenData.html",Token:"script/ast/struct.Token.html",ExprDef:"script/ast/type.ExprDef.html",Position:"script/ast/struct.Position.html",Expr:"script/ast/struct.Expr.html",BinOp:"script/ast/type.BinOp.html",LogOp:"script/ast/type.LogOp.html",CompOp:"script/ast/type.CompOp.html",BitOp:"script/ast/type.BitOp.html",UnaryOp:"script/ast/type.UnaryOp.html",NumOp:"script/ast/type.NumOp.html",Const:"script/ast/type.Const.html",};
+
+                if (window.register_implementors) {
+                    window.register_implementors(implementors);
+                } else {
+                    window.pending_implementors = implementors;
+                }
+            
+})()
