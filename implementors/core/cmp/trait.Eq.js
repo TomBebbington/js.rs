a6d56869fa3e50d440da3a0c6a909ca894a3c1d3
Tom Bebbington <tophattedcoder@gmail.com>
2014-05-25 01:52:50 +0100

    Update docs to reflect rename

--- implementors/core/cmp/trait.Eq.js
+++ implementors/core/cmp/trait.Eq.js
@@ -1,6 +1,8 @@
 (function() {var implementors = {};
 implementors['script'] = {ValueData:"script/js/value/type.ValueData.html",ParseError:"script/parser/type.ParseError.html",NumberType:"script/lexer/type.NumberType.html",CommentType:"script/lexer/type.CommentType.html",StringType:"script/lexer/type.StringType.html",TokenData:"script/ast/type.TokenData.html",Token:"script/ast/struct.Token.html",ExprDef:"script/ast/type.ExprDef.html",Position:"script/ast/struct.Position.html",Expr:"script/ast/struct.Expr.html",BinOp:"script/ast/type.BinOp.html",LogOp:"script/ast/type.LogOp.html",CompOp:"script/ast/type.CompOp.html",BitOp:"script/ast/type.BitOp.html",UnaryOp:"script/ast/type.UnaryOp.html",NumOp:"script/ast/type.NumOp.html",Const:"script/ast/type.Const.html",};
 
+implementors['js'] = {ValueData:"js/js/value/type.ValueData.html",ParseError:"js/parser/type.ParseError.html",NumberType:"js/lexer/type.NumberType.html",CommentType:"js/lexer/type.CommentType.html",StringType:"js/lexer/type.StringType.html",TokenData:"js/ast/type.TokenData.html",Token:"js/ast/struct.Token.html",ExprDef:"js/ast/type.ExprDef.html",Position:"js/ast/struct.Position.html",Expr:"js/ast/struct.Expr.html",BinOp:"js/ast/type.BinOp.html",LogOp:"js/ast/type.LogOp.html",CompOp:"js/ast/type.CompOp.html",BitOp:"js/ast/type.BitOp.html",UnaryOp:"js/ast/type.UnaryOp.html",NumOp:"js/ast/type.NumOp.html",Const:"js/ast/type.Const.html",};
+
                 if (window.register_implementors) {
                     window.register_implementors(implementors);
                 } else {
--- implementors/core/ops/trait.Div.js
+++ implementors/core/ops/trait.Div.js
@@ -1,6 +1,8 @@
 (function() {var implementors = {};
 implementors['script'] = {ValueData:"script/js/value/type.ValueData.html",};
 
+implementors['js'] = {ValueData:"js/js/value/type.ValueData.html",};
+
                 if (window.register_implementors) {
                     window.register_implementors(implementors);
                 } else {
