0ac077f838af4adb1ba9db43203e3690c45846a2
Tom Bebbington <tophattedcoder@gmail.com>
2014-05-26 11:53:09 +0100

    Add documentation to LibJIT

--- Makefile
+++ Makefile
@@ -20,6 +20,7 @@ install:
 	sudo cp -f target/libjs*.so /usr/local/lib
 	-sudo ln -s /usr/local/bin/js.rs /usr/bin/js.rs
 doc:
+	$(RUSTDOC) src/libjit/lib.rs -o doc -L target
 	$(RUSTDOC) src/libjs/lib.rs -o doc -L target
 	$(RUSTDOC) src/libjs_syntax/lib.rs -o doc -L target
 update-doc: doc
--- src/libjit/lib.rs
+++ src/libjit/lib.rs
@@ -27,9 +27,11 @@
 #![comment = "LibJIT Bindings"]
 #![crate_type = "dylib"]
 #![crate_type = "rlib"]
-#![allow(missing_doc)]
+#![deny(missing_doc)]
 #![allow(raw_pointer_deriving)]
 #![stable]
+
+//! This crate wraps LibJIT
 
 extern crate libc;
 extern crate core;
@@ -38,14 +40,18 @@ extern crate core;
 use std::ptr;
 use core::mem::transmute;
 use libc::{FILE, STDOUT_FILENO, c_int, fdopen, c_void, c_uint, c_char, c_float};
-
+/// A platform's application binary interface
 pub enum ABI {
+    /// The C application binary interface
     CDECL = 0
 }
-
+/// Call flags to a function
 pub enum CallFlags {
+    /// When the function won't throw a value
     JitCallNothrow = 1,
+    /// When the function won't return a value
     JitCallNoReturn = 2,
+    /// When the function is tail-recursive
     JitCallTail = 4,
 }
 
@@ -101,31 +107,32 @@ extern {
     static jit_type_float64: *c_void;
     static jit_type_void_ptr: *c_void;
 }
-
+/// Holds all of the functions you have built and compiled. There can be multuple, but normally there is only one.
 pub struct Context {
     _context: *c_void
 }
 
 impl Context {
+    /// Create a new JIT Context
     pub fn new() -> Box<Context> {
         unsafe {
             let context = jit_context_create();
             box Context { _context: context }
         }
     }
-
+    /// Lock down the context to prevent multiple threads from using the builder at a time
     pub fn build_start(&self) {
         unsafe {
             jit_context_build_start(self._context);
         }
     }
-
+    /// Unlock the context from this thread
     pub fn build_end(&self) {
         unsafe {
             jit_context_build_end(self._context);
         }
     }
-
+    /// Create a function in the context with the type signature given
     pub fn create_function(&self, signature: &Type) -> Box<Function> {
         unsafe {
             let function = jit_function_create(self._context, signature._type);
@@ -141,12 +148,13 @@ impl Drop for Context {
         }
     }
 }
-
+/// A Type of a value to JIT
 pub struct Type {
     _type: *c_void
 }
 
 impl Type {
+    /// Create a function signature, with the given ABI, return type and parameters
     pub fn create_signature(abi: ABI, return_type: &Type, params: &[&Type]) -> Box<Type> {
         unsafe {
             let mut ps: Vec<*c_void> = vec!();
@@ -176,30 +184,30 @@ impl Type {
             box Type { _type: ty }
         }
     }
-
+    /// Create a struct type with the given field types
     pub fn create_struct(fields: &[&Type]) -> Box<Type> {
         Type::create_complex(fields, false)
     }
-
+    /// Create a union type with the given field types
     pub fn create_union(fields: &[&Type]) -> Box<Type> {
         let inner = Type::create_complex(fields, true);
         Type::create_struct(&[&*Types::get_int(), &*inner])
     }
-
+    /// Create a pointer type with the given pointee type
     pub fn create_pointer(pointee: &Type) -> Box<Type> {
         unsafe {
             let ptr = jit_type_create_pointer(pointee._type, 1);
             box Type { _type: ptr }
         }
     }
-
+    /// Work out the size of this type
     pub fn get_size(&self) -> c_uint {
         unsafe {
             jit_type_get_size(self._type)
         }
     }
 }
-
+/// A Function to JIT
 pub struct Function {
     _context: *Context,
     _function: *c_void
@@ -219,7 +227,7 @@ impl Function {
             box Value { _value: value }
         }
     }
-
+    /// Dump the function onto stdout
     pub fn dump(&self, name: &str) {
         unsafe {
             let c_str = name.to_c_str().unwrap();
@@ -228,105 +236,105 @@ impl Function {
             jit_dump_function(stdout, self._function, c_str);
         }
     }
-
+    /// Compile the function
     pub fn compile(&self) {
         unsafe {
             jit_function_compile(self._function);
         }
     }
-
+    /// Get a parameter of the function as a JIT Value
     pub fn get_param(&self, param: uint) -> Value {
         unsafe {
             let value = jit_value_get_param(self._function, param as c_uint);
             Value { _value: value }
         }
     }
-
+    /// Return from the function with the value given
     pub fn insn_return(&self, retval: &Value) {
         unsafe {
             jit_insn_return(self._function, retval._value);
         }
     }
-
+    /// Make an instruction that multiplies the values
     pub fn insn_mul(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_mul)
     }
-
+    /// Make an instruction that adds the values
     pub fn insn_add(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_add)
     }
-
+    /// Make an instruction that subtracts the second value from the first
     pub fn insn_sub(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_sub)
     }
-
+    /// Make an instruction that divides the first number by the second
     pub fn insn_div(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_div)
     }
-
+    /// Make an instruction that checks if the first value is lower than or equal to the second
     pub fn insn_leq(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_le)
     }
-
+    /// Make an instruction that checks if the first value is greater than or equal to the second
     pub fn insn_geq(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_ge)
     }
-
+    /// Make an instruction that checks if the first value is lower than the second
     pub fn insn_lt(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_lt)
     }
-
+    /// Make an instruction that checks if the first value is greater than the second
     pub fn insn_gt(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_gt)
     }
-
+    /// Make an instruction that checks if the values are equal
     pub fn insn_eq(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_eq)
     }
-
+    /// Make an instruction that checks if the values are not equal
     pub fn insn_neq(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_ne)
     }
-
+    /// Make an instruction that performs a bitwise and on the two values
     pub fn insn_and(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_and)
     }
-
+    /// Make an instruction that performs a bitwise or on the two values
     pub fn insn_or(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_or)
     }
-
+    /// Make an instruction that performs a bitwise xor on the two values
     pub fn insn_xor(&self, v1: &Value, v2: &Value) -> Box<Value> {
         self.insn_binop(v1, v2, jit_insn_xor)
     }
-
+    /// Make an instruction that performs a bitwise not on the two values
     pub fn insn_not(&self, value: &Value) -> Box<Value> {
         self.insn_unop(value, jit_insn_not)
     }
-
+    /// Make an instruction that performs a bitwise negate on the value
     pub fn insn_neg(&self, value: &Value) -> Box<Value> {
         self.insn_unop(value, jit_insn_neg)
     }
-
+    /// Make an instruction that duplicates the value given
     pub fn insn_dup(&self, value: &Value) -> Box<Value> {
         unsafe {
             let dup_value = jit_insn_load(self._function, value._value);
             box Value { _value: dup_value }
         }
     }
-
+    /// Make an instruction that stores a value at a destination value
     pub fn insn_store(&self, dest: &Value, src: &Value) {
         unsafe {
             jit_insn_store(self._function, dest._value, src._value);
         }
     }
-
+    /// Make an instruction that stores a value a certain offset away from a destination value
     pub fn insn_store_relative(&self, dest: &Value, offset: c_int, src: &Value) {
         unsafe {
             jit_insn_store_relative(self._function, dest._value, offset, src._value);
         }
     }
-
+    /// Make an instruction that sets a label
     pub fn insn_set_label(&self, label: &mut Label) {
         unsafe {
             // TODO: messy; I suspect transmute is REALLY BAD PRACTICE
@@ -334,28 +342,28 @@ impl Function {
             jit_insn_label(self._function, label_ptr);
         }
     }
-
+    /// Make an instruction that branches to a certain label
     pub fn insn_branch(&self, label: &mut Label) {
         unsafe {
             let label_ptr: *mut c_void = transmute(&label._label);
             jit_insn_branch(self._function, label_ptr);
         }
     }
-
+    /// Make an instruction that branches to a certain label if the value is true
     pub fn insn_branch_if(&self, value: &Value, label: &mut Label) {
         unsafe {
             let label_ptr: *mut c_void = transmute(&label._label);
             jit_insn_branch_if(self._function, value._value, label_ptr);
         }
     }
-
+    /// Make an instruction that branches to a certain label if the value is false
     pub fn insn_branch_if_not(&self, value: &Value, label: &mut Label) {
         unsafe {
             let label_ptr: *mut c_void = transmute(&label._label);
             jit_insn_branch_if_not(self._function, value._value, label_ptr);
         }
     }
-
+    /// Make an instruction that calls a native function that has the signature given with some arguments
     fn insn_call_native(&self, name: &'static str, native_func: *u8,
                         signature: &Type, args: &[&Value]) -> Box<Value> {
         unsafe {
@@ -375,59 +383,59 @@ impl Function {
             })
         }
     }
-
+    /// Make an instruction that calls a Rust function that has the signature given with no arguments and expects a return value
     pub fn insn_call_native0<R>(&self, name: &'static str,
                                 native_func: fn() -> R,
                                 signature: &Type, args: &[&Value]) -> Box<Value> {
         self.insn_call_native(name, unsafe { transmute(native_func) }, signature, args)
     }
-
+    /// Make an instruction that calls a Rust function that has the signature given with a single argument and expects a return value
     pub fn insn_call_native1<A,R>(&self, name: &'static str,
                                   native_func: fn(A) -> R,
                                   signature: &Type, args: &[&Value]) -> Box<Value> {
         self.insn_call_native(name, unsafe { transmute(native_func) }, signature, args)
     }
-
+    /// Make an instruction that allocates some space
     pub fn insn_alloca(&self, size: &Value) -> Box<Value> {
         unsafe {
             box Value { _value: jit_insn_alloca(self._function, size._value) }
         }
     }
-
+    /// Apply a function to some arguments and set the retval to the return value
     pub fn apply<T>(&self, args: &[*c_void], retval: &mut T) {
         unsafe {
             let pargs = args;
             jit_function_apply(self._function, pargs.as_ptr(), transmute(retval));
         }
     }
-
+    /// Execute a function and with some arguments
     pub fn execute(&self, args: &[*c_void]) {
         unsafe {
             let pargs = args;
             jit_function_apply(self._function, pargs.as_ptr(), ptr::mut_null());
         }
     }
-
+    /// Turn this function into a closure
     pub fn closure<T>(&self) -> T {
         unsafe {
             transmute(jit_function_to_closure(self._function))
         }
     }
-
+    /// Create a 32-bit float constant in this context
     pub fn constant_float32(&self, constant: f32) -> Box<Value> {
         unsafe {
             let value = jit_value_create_float32_constant(self._function, jit_type_float32, constant);
             box Value { _value: value }
         }
     }
-
+    /// Create a 32-bit integer constant in this context
     pub fn constant_int32(&self, constant: i32) -> Box<Value> {
         unsafe {
             let value = jit_value_create_nint_constant(self._function, jit_type_int, constant);
             box Value { _value: value }
         }
     }
-
+    /// Create a new value with the given type
     pub fn create_value(&self, value_type: &Type) -> Box<Value> {
         unsafe {
             let value = jit_value_create(self._function, value_type._type);
@@ -435,13 +443,14 @@ impl Function {
         }
     }
 }
-
 #[deriving(Clone)]
+/// A Value that is being JITed
 pub struct Value {
     _value: *c_void
 }
 
 #[deriving(Hash, Eq)]
+/// A label in the code that can be branched to in instructions
 pub struct Label {
     _label: *c_void
 }
@@ -450,30 +459,31 @@ impl Label {
     fn undefined() -> *c_void {
         !0u32 as *c_void
     }
-
+    /// Create a new label
     pub fn new() -> Box<Label> {
         box Label { _label: Label::undefined() }
     }
 }
-
+/// Quick retrieval of common types
 pub struct Types;
 impl Types {
+    /// Void type
     pub fn get_void() -> Box<Type> {
         box Type { _type: jit_type_void }
     }
-
+    /// Integer type
     pub fn get_int() -> Box<Type> {
         box Type { _type: jit_type_int }
     }
-
+    /// 32-bit floating point type
     pub fn get_float32() -> Box<Type> {
         box Type { _type: jit_type_float32 }
     }
-
+    /// 64-bit floating point type
     pub fn get_float64() -> Box<Type> {
         box Type { _type: jit_type_float64 }
     }
-
+    /// A void pointer, which can represent any kind of pointer
     pub fn get_void_ptr() -> Box<Type> {
         box Type { _type: jit_type_void_ptr }
     }
