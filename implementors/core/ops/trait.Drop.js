4aab63db334f50e6477e05bf937215dcf7c4a12b
Tom Bebbington <tophattedcoder@gmail.com>
2014-05-28 20:21:27 +0100

    Auto-update docs

--- implementors/core/ops/trait.Drop.js
+++ implementors/core/ops/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['jit'] = {Type:"jit/struct.Type.html",Context:"jit/struct.Context.html",};
+implementors['jit'] = {Function:"jit/struct.Function.html",Type:"jit/struct.Type.html",Context:"jit/struct.Context.html",};
 
                 if (window.register_implementors) {
                     window.register_implementors(implementors);
