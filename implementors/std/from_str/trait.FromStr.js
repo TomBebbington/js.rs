ebfd0c449a312409332b7a26abcc1ee7b7f32afe
Tom Bebbington <tophattedcoder@gmail.com>
2014-05-30 15:31:45 +0100

    Auto-update docs

---
+++ implementors/std/from_str/trait.FromStr.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['js_syntax'] = {Keyword:"js_syntax/ast/keyword/type.Keyword.html",};
+
+                if (window.register_implementors) {
+                    window.register_implementors(implementors);
+                } else {
+                    window.pending_implementors = implementors;
+                }
+            
+})()
