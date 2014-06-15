c321b2ea7913925935ba53b4a12402e3bce41408
Tom Bebbington <tophattedcoder@gmail.com>
2014-06-15 15:57:16 +0100

    Auto-update docs

--- implementors/js/run/compiler/trait.Compiler.js
+++ implementors/js/run/compiler/trait.Compiler.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['js_jit'] = ["impl&lt;'a&gt; <a class='trait' href='js/run/compiler/trait.Compiler.html' title='js::run::compiler::Compiler'>Compiler</a>&lt;'a, <a href='http://doc.rust-lang.org/std/primitive.tuple.html'>(Value&lt;'a&gt;, &amp;'a Function&lt;'a&gt;)</a>&gt; for <a class='struct' href='js_jit/struct.JitCompiler.html' title='js_jit::JitCompiler'>JitCompiler</a>&lt;'a&gt;",];
+implementors['js_jit'] = ["impl&lt;''a&gt; <a class='trait' href='js/run/compiler/trait.Compiler.html' title='js::run::compiler::Compiler'>Compiler</a>&lt;''a, <a href='http://doc.rust-lang.org/std/primitive.tuple.html'>(Value&lt;''a&gt;, &amp;''a Function&lt;''a&gt;)</a>&gt; for <a class='struct' href='js_jit/struct.JitCompiler.html' title='js_jit::JitCompiler'>JitCompiler</a>&lt;''a&gt;",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
