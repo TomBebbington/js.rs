0c230bc94ab9a14b672cee5bc742bda4b91ebdf4
Tom Bebbington <tophattedcoder@gmail.com>
2014-06-27 19:47:32 +0100

    Auto-update docs

--- implementors/core/ops/trait.BitXor.js
+++ implementors/core/ops/trait.BitXor.js
@@ -1,5 +1,6 @@
 (function() {var implementors = {};
 implementors['front'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/ops/trait.BitXor.html' title='core::ops::BitXor'>BitXor</a>&lt;<a class='struct' href='front/stdlib/value/struct.Value.html' title='front::stdlib::value::Value'>Value</a>, <a class='struct' href='front/stdlib/value/struct.Value.html' title='front::stdlib::value::Value'>Value</a>&gt; for <a class='struct' href='front/stdlib/value/struct.Value.html' title='front::stdlib::value::Value'>Value</a>",];
+implementors['js'] = ["impl <a class='trait' href='http://doc.rust-lang.org/core/ops/trait.BitXor.html' title='core::ops::BitXor'>BitXor</a>&lt;<a class='struct' href='js/front/stdlib/value/struct.Value.html' title='js::front::stdlib::value::Value'>Value</a>, <a class='struct' href='js/front/stdlib/value/struct.Value.html' title='js::front::stdlib::value::Value'>Value</a>&gt; for <a class='struct' href='js/front/stdlib/value/struct.Value.html' title='js::front::stdlib::value::Value'>Value</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
