4c37e141aa7d907127ce5609968d755647368e21
Tom Bebbington <tophattedcoder@gmail.com>
2014-06-30 01:00:15 +0100

    Change & back into *

--- src/lib/back/executor.rs
+++ src/lib/back/executor.rs
@@ -54,7 +54,7 @@ fn convert_to_value<'a>(func:&Function<'a>, val:&'a JITVal<'a>) -> JITVal<'a> {
     let val_kind = val_type.get_kind();
     if val_kind.contains(SysBool) || val_kind.contains(UByte) {
         let bool_value = to_value::<bool>;
-        let sig = get_type::<fn(bool) -> &int>();
+        let sig = get_type::<fn(bool) -> *mut int>();
         func.insn_call_native1("bool_value", bool_value, &sig, &mut [val])
     } else if val_kind.contains(Pointer) {
         let ref_t = val_type.get_ref();
@@ -65,7 +65,7 @@ fn convert_to_value<'a>(func:&Function<'a>, val:&'a JITVal<'a>) -> JITVal<'a> {
                     to_value(text.as_str().unwrap().into_string())
                 }
             }
-            let sig = get_type::<fn(String) -> &int>();
+            let sig = get_type::<fn(String) -> *mut int>();
             func.insn_call_native1("string_value", string_value, &sig, &mut [val])
         } else {
             fn ptr_value(ptr: &i8) -> JSVal {
@@ -77,22 +77,22 @@ fn convert_to_value<'a>(func:&Function<'a>, val:&'a JITVal<'a>) -> JITVal<'a> {
                     ptr => fail!("Invalid pointer: {}", ptr)
                 }
             }
-            let sig = get_type::<fn(&i8) -> &int>();
+            let sig = get_type::<fn(&i8) -> *mut int>();
             func.insn_call_native1("ptr_value", ptr_value, &sig, &mut [val])
         }
     } else if val_kind.contains(Int) || val_kind.contains(UInt) {
         let int_value = to_value::<i32>;
-        let sig = get_type::<fn(i32) -> &int>();
+        let sig = get_type::<fn(i32) -> *mut int>();
         func.insn_call_native1("int_value", int_value, &sig, &mut [val])
     } else if val_kind.contains(NInt) || val_kind.contains(NUInt) {
         fn sys_int_value(num:int) -> JSVal {
             to_value::<i32>(num as i32)
         }
-        let sig = get_type::<fn(int) -> &int>();
+        let sig = get_type::<fn(int) -> *mut int>();
         func.insn_call_native1("sys_int_value", sys_int_value, &sig, &mut [val])
     } else if val_kind.contains(Float64) {
         let float_value = to_value::<f64>;
-        let sig = get_type::<fn(f64) -> &int>();
+        let sig = get_type::<fn(f64) -> *mut int>();
         func.insn_call_native1("float_value", float_value, &sig, &mut [val])
     } else {
         fail!("Invalid kind {}", val_kind.bits())
